a20924703f579105c66bb8dc239755d733851959
Gabriel Gustavo Nicolodi <gabriel.gnicolodi@gmail.com>
2024-11-21 01:08:31 -0300

    Solving exercices 1,2,3 and 5, almost finishing 6

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,11 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+BLOCK=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 256128))
+BLOCK_TXS=$(echo $BLOCK | jq .tx)
+for tx in $(echo $BLOCK_TXS | jq -r '.[]'); do
+    TRANSACTION=$(bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction $tx))
+    if echo $TRANSACTION | jq 'select(.vin | map(select(.coinbase != null)) | length > 0) | .' > /dev/null; then
+        COINBASE_TX=$tx
+        break
+    fi
+done
+
