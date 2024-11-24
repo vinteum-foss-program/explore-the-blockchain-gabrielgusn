9c8b35f927b7a9cf43d60628caded2343844a4ce
Gabriel Gustavo Nicolodi <gabriel.gnicolodi@gmail.com>
2024-11-24 01:32:00 -0300

    Solving exercise 6 and starting 8

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,4 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+TRANSACTION=$(bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction "e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"))
+echo $TRANSACTION
