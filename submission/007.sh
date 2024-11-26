8489bb207deb5782afdbabe0c09a96ae8c1d9dde
Gabriel Gustavo Nicolodi <gabriel.gnicolodi@gmail.com>
2024-11-26 00:38:12 -0300

    Solving exercise 7 but in a way that hopefully will not exceed the pipeline timeout

--- submission/007.sh
+++ submission/007.sh
@@ -1,20 +1,24 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
 #!/bin/bash
 
+
 BLOCK_HEIGHT=123321
 
-BLOCK=$(bitcoin-cli getblock $(bitcoin-cli getblockhash $BLOCK_HEIGHT) | jq -r '.tx')
+BLOCK=$(bitcoin-cli getblock $(bitcoin-cli getblockhash $BLOCK_HEIGHT) 2 | jq  -c '.tx')
 
-for TXID in $(echo "$BLOCK" | jq -r '.[]'); do
+# Deixei "hard coded" nesse momento pois a pipeline não estava passando por conta de timeout na resposta do node
+# BLOCK=$(cat block.json | jq -c '.tx')
 
-  TX_DETAILS=$(bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction "$TXID"))
-  TX_VOUT_AMOUNT=$(echo $TX_DETAILS | jq .vout | jq length)
-
+echo $BLOCK | jq -c '.[]' | while read -r transaction; do
+  TXID=$(echo $transaction | jq -r '.hash')
+  TX_VOUT=$(echo $transaction | jq -r '.vout')
+  TX_VOUT_AMOUNT=$(echo $TX_VOUT | jq length)
   for (( i=0; i<TX_VOUT_AMOUNT; i++)); do
     UNSPENT_OUTPUT=$(bitcoin-cli gettxout "$TXID" $i)
-    [[ -n "$UNSPENT_OUTPUT" ]] && echo $(echo $UNSPENT_OUTPUT | jq -r '.scriptPubKey.address') && break
 
+    if [ -n "$UNSPENT_OUTPUT" ]; then
+      echo $(echo $UNSPENT_OUTPUT | jq -r '.scriptPubKey.address')
+      exit 0
+    fi
   done
-
-
 done
