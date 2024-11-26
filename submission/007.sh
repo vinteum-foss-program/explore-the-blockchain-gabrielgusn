688159aa522e48030bb06cfa79da7170cf413ea7
Gabriel Gustavo Nicolodi <gabriel.gnicolodi@gmail.com>
2024-11-25 23:59:31 -0300

    Solving exercise 7

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,20 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+#!/bin/bash
+
+BLOCK_HEIGHT=123321
+
+BLOCK=$(bitcoin-cli getblock $(bitcoin-cli getblockhash $BLOCK_HEIGHT) | jq -r '.tx')
+
+for TXID in $(echo "$BLOCK" | jq -r '.[]'); do
+
+  TX_DETAILS=$(bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction "$TXID"))
+  TX_VOUT_AMOUNT=$(echo $TX_DETAILS | jq .vout | jq length)
+
+  for (( i=0; i<TX_VOUT_AMOUNT; i++)); do
+    UNSPENT_OUTPUT=$(bitcoin-cli gettxout "$TXID" $i)
+    [[ -n "$UNSPENT_OUTPUT" ]] && echo $(echo $UNSPENT_OUTPUT | jq -r '.scriptPubKey.address') && break
+
+  done
+
+
+done
